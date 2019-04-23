817b66a06566207933554f6cdd5a1ce739f986b0
kazlauskis <karolis@kazlauskis.com>
2019-04-23 09:47:32 +0100

    Fix heroku build

--- config/server.js
+++ config/server.js
@@ -1,18 +1,11 @@
-const PORT = 8000;
-let NETWORK = 'localhost';
-
-if (process.argv.length >= 2) {
-  if (process.argv.indexOf('mobile') >= 2) {
-    NETWORK = '192.168.1.1';
-  }
-}
-
+const PORT = process.env.PORT || 8000;
 const express = require('express');
+
 const app = express();
 
 app.use(express.static('dist/main'));
 
-const server = app.listen(PORT, NETWORK, () => {
+const server = app.listen(PORT, () => {
   const host = server.address().address;
   const port = server.address().port;
 
