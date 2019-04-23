8f327ad9a63f3e81999bd4b930fdfd42009f9d73
kazlauskis <karolis@kazlauskis.com>
2019-04-23 07:02:22 +0100

    Fix heroku build

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -9,11 +9,11 @@ const ENV = process.env.NODE_ENV || process.env.ENV || 'development';
 
 module.exports = webpackMerge(commonConfig, {
   // Extend common/config/config.js file with common/config/config_dev.js
-  resolve: {
-    alias: {
-      config: 'common/config/config_dev',
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     config: 'common/config/config_dev',
+  //   },
+  // },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
