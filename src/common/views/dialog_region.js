f8de27c75d35b6d3ce0018230999907fd3b41c57
kazlauskis <karolis@kazlauskis.com>
2019-07-30 14:47:08 +0100

    Fix dialog close issues

--- src/common/views/dialog_region.js
+++ src/common/views/dialog_region.js
@@ -115,7 +115,7 @@ export default Marionette.Region.extend({
 
     // attach events
     this.on('view:show', this.showModal, this);
-    this.$el.on('click #dialog', this._onContainerClick);
+    this.$el.on('click #dialog', this._onContainerClick.bind(this));
   },
 
   hideAllowed: true, // hide the dialog on clicking the container
@@ -211,7 +211,7 @@ export default Marionette.Region.extend({
         {
           id: 'ok',
           title: 'OK',
-          onClick: this.hide,
+          onClick: this.hide.bind(this),
         },
       ],
     };
