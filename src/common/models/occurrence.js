da2d768bbb8ec9c7946a30e1a61fb8d5bd61d2df
Karolis Kazlauskis <karolis@flumens.io>
2021-05-03 08:20:52 +0100

    Add training flag to occurrences

--- src/common/models/occurrence.js
+++ src/common/models/occurrence.js
@@ -1,12 +1,19 @@
 import Indicia from 'indicia';
 import _ from 'lodash';
 import CONFIG from 'config';
+import appModel from 'app_model';
 import ImageModel from './image'; // eslint-disable-line
 
 export default Indicia.Occurrence.extend({
   Media: ImageModel,
 
   keys: CONFIG.indicia.occurrence, // warehouse attribute keys
+
+  defaults() {
+    return {
+      training: appModel.get('useTraining') ? 't' : 'f',
+    };
+  },
 
   validateRemote(attributes) {
     const attrs = _.extend({}, this.attributes, attributes);
