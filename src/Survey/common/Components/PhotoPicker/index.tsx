a6428ee4a368b98257310ce977e4a4a02576d001
Karolis Kazlauskis <karolis@flumens.io>
2024-10-04 11:03:57 +0300

    Add photo captions

--- src/Survey/common/Components/PhotoPicker/index.tsx
+++ src/Survey/common/Components/PhotoPicker/index.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react';
 import { Capacitor } from '@capacitor/core';
-import { PhotoPicker, captureImage } from '@flumens';
+import { Gallery, PhotoPicker, captureImage, useAlert } from '@flumens';
 import { isPlatform } from '@ionic/react';
 import config from 'common/config';
 import Media from 'models/image';
@@ -11,6 +11,72 @@ import './styles.scss';
 type Props = {
   model: Sample | Occurrence;
 };
+
+type GalleryProps = {
+  items: Media[];
+  showGallery: number;
+  onClose: () => void;
+};
+
+const GalleryDefaultWrap = observer(
+  ({ items: itemsProp, showGallery, ...props }: GalleryProps) => {
+    const alert = useAlert();
+    const getItem = (image: Media) => {
+      const { caption } = image.attrs;
+
+      const onCaptionChange = () => {
+        if (image.isDisabled()) return;
+        alert({
+          header: 'Edit',
+          skipTranslation: true,
+          message: '',
+          buttons: [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+            },
+            {
+              text: 'Save',
+              handler(values) {
+                const newValue = values[0] || '';
+                // eslint-disable-next-line no-param-reassign
+                image.attrs.caption = newValue;
+              },
+            },
+          ],
+          inputs: [
+            {
+              type: 'textarea',
+              placeholder: 'Enter your caption here...',
+              value: caption,
+            },
+          ],
+        });
+      };
+
+      return {
+        src: 'getURL' in image ? image.getURL() : (image as any).data!,
+        footer: (
+          <div
+            onClick={onCaptionChange}
+            className="w-full rounded-md bg-white/10 p-1 text-white"
+          >
+            {caption || 'Photo caption...'}
+          </div>
+        ),
+      };
+    };
+
+    return (
+      <Gallery
+        isOpen={Number.isFinite(showGallery)}
+        items={itemsProp.map(getItem)}
+        initialSlide={showGallery}
+        {...props}
+      />
+    );
+  }
+);
 
 const AppPhotoPicker = ({ model }: Props) => {
   const isUploaded = model.isUploaded();
@@ -47,6 +113,7 @@ const AppPhotoPicker = ({ model }: Props) => {
       onRemove={onRemove}
       value={model.media}
       isDisabled={isUploaded}
+      Gallery={GalleryDefaultWrap}
     />
   );
 };
